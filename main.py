6dc2f36dbe37868aa248f66b7cbcd3fcf2eddef3
Nutthawat Chotpanjawong <aungodzilla2548@gmail.com>
2023-10-01 02:37:11 +0700

    ปรับโค้ดให้มีfunctionและclass

--- main.py
+++ main.py
@@ -1,62 +1,65 @@
 import pygame
 from pygame import mixer
 
-#เรียกใช้งาน pygame, audio
-pygame.init()
-mixer.init()
+class Mainmenu:
+    def __init__(self):
+        #เรียกใช้งาน pygame, audio
+        pygame.init()
+        mixer.init()
 
-#หน้าจอโปรแกรม
-SCREEN_W = 1280
-SCREEN_H = 720
-pygame.display.set_mode((SCREEN_W, SCREEN_H))
-screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+        #หน้าจอโปรแกรม
+        self.SCREEN_W = 1280
+        self.SCREEN_H = 720
+        self.screen = pygame.display.set_mode((self.SCREEN_W, self.SCREEN_H))
 
-#ขัอความและฟอนต์
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-fontcolor_mainmenu = (180, 180, 179)
-sys_font = pygame.font.SysFont("capriolaregular", 108)
-message_text = sys_font.render("JustType", True, fontcolor_mainmenu)
+        #ขัอความและฟอนต์
+        self.fontcolor_mainmenu = (180, 180, 179)
+        self.sys_font = pygame.font.SysFont("capriolaregular", 108)
+        self.message_text = self.sys_font.render("JustType", True, self.fontcolor_mainmenu)
 
-#ไอคอนและชื่อโปรแกรม
-icon = pygame.image.load("images/icon.png")
-pygame.display.set_icon(icon)
-pygame.display.set_caption("JustType")
+        #ไอคอนและชื่อโปรแกรม
+        self.icon = pygame.image.load("images/icon.png")
+        pygame.display.set_icon(self.icon)
+        pygame.display.set_caption("JustType")
 
-#dinosaur ลอยไปมา
-dinosaur_img = pygame.image.load("images/dinosaur.png")
-dinosaur_img = pygame.transform.scale(dinosaur_img, (200, 300))
+        #dinosaur ลอยไปมา
+        self.dinosaur_img = pygame.image.load("images/dinosaur.png")
+        self.dinosaur_img = pygame.transform.scale(self.dinosaur_img, (200, 300))
 
-#ปุ่ม Start
-start_button = pygame.Rect(540, 400, 200, 50)
-font_button = pygame.font.SysFont("capriolaregular", 36)
-text_startbutton = font_button.render("Start", True, (255, 255, 255))
-text_rect = text_startbutton.get_rect(center=start_button.center)
+        #ปุ่ม Start
+        self.start_button = pygame.Rect(540, 400, 200, 50)
+        self.font_button = pygame.font.SysFont("capriolaregular", 36)
+        self.text_startbutton = self.font_button.render("Start", True, (255, 255, 255))
+        self.text_rect = self.text_startbutton.get_rect(center=self.start_button.center)
 
-#เสียง mainmenu audio
-mixer.music.load("audio/mainmenu.mp3")
-mixer.music.set_volume(0.3)
-mixer.music.play(loops=1)
+        #เสียง mainmenu audio
+        mixer.music.load("audio/mainmenu.mp3")
+        mixer.music.set_volume(0.3)
+        mixer.music.play(loops=1)
 
-flyspeed = 0
-running = True
-while running:
-    for event in pygame.event.get():
-        #กากบาทปิดจบการทำงาน
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-        #กด ESC ปิดจบการทำงาน
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                pygame.quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if start_button.collidepoint(event.pos):
-                pass
-    screen.blit(dinosaur_img, (flyspeed, 0))
-    screen.blit(message_text, (420, 200))
-    pygame.draw.rect(screen, (0, 128, 255), start_button)
-    screen.blit(text_startbutton, text_rect)
-    flyspeed += 0.2
-    pygame.display.flip()
+        self.flyspeed = 0
+        self.running = True
+
+    def runprogram(self):
+        while self.running:
+            for event in pygame.event.get():
+                #กากบาทปิดจบการทำงาน
+                if event.type == pygame.QUIT:
+                    self.running = False
+                #กด ESC ปิดจบการทำงาน
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.running = False
+
+            self.screen.blit(self.dinosaur_img, (self.flyspeed, (self.SCREEN_H)-780))
+            self.screen.blit(self.message_text, (420, 200))
+            pygame.draw.rect(self.screen, (0, 128, 255), self.start_button)
+            self.screen.blit(self.text_startbutton, self.text_rect)
+            self.flyspeed += 0.2
+            pygame.display.flip()
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Mainmenu()
+    game.runprogram()
