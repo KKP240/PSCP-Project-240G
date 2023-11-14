3e3fd4cd4c015deef1075e388336b1dbd9d3e793
KKP240 <141379356+KKP240@users.noreply.github.com>
2023-11-14 14:45:13 +0700

    Update script_menu.js
    slightly update

--- PSCP-Project/script_menu.js
+++ PSCP-Project/script_menu.js
@@ -4,9 +4,16 @@ document.getElementById('start-button').addEventListener('click', function() {
 
 let cartoon = document.getElementById('cartoon');
 let runspeed = 0;
+var audio = document.getElementById("audio");
+    audio.volume = 0.12;
 
 function animateCartoon() {
-    runspeed += 0.18;
+    runspeed += 0.28;
+    
+    if (runspeed >= window.innerWidth) {
+        runspeed = 0;
+    }
+
     cartoon.style.left = runspeed + 'px';
     requestAnimationFrame(animateCartoon);
 }
